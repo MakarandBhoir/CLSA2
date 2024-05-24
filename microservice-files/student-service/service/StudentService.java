0fe04efe5d5f51ee983247a94ece52660122aa7d
makarand.bhoir <makarand.bhoir@live.com>
2024-05-24 14:09:40 +0530

    microservice files

---
+++ microservice-files/student-service/service/StudentService.java
@@ -0,0 +1,11 @@
+package com.clsa.service;
+
+import java.util.List;
+
+import com.clsa.model.Student;
+
+public interface StudentService {
+	public List<Student> findAllStudents();
+
+	public Student findStudentById(Integer studentId);
+}
